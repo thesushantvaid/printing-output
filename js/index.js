8bdce1f11600725de2b3aa4a9df86ff27b3b46b7
thesushantvaid <132909409+thesushantvaid@users.noreply.github.com>
2023-05-16 23:35:58 -0400

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,74 @@
+//TASK 1 ----------------------------------------------------------------------------
+
+let a = 3;
+let b = 5;
+let c;
+
+// alert( "description of alerted_value: " + alerted_value );
+
+let print = 
+`
+let a = 3;
+let b = 5;
+let c;
+------------
+a + b =${a + b}
+a - b =${a - b}
+a * b =${a * b}
+a / b =${a / b}
+a % b =${a % b}
+a += b =${a += b}
+a -= b =${a -= b}
+a *= b =${a *= b}
+a /= b =${a /= b}
+a %= b =${a %= b}
+a == b =${a == b}
+a != b =${a != b}
+a > b =${a > b}
+a < b =${a < b}
+!a && !c =${!a && !c}
+!a || !c =${!a || !c}
+`
+alert(print);
+
+/*
+a + b
+a - b
+a * b
+a / b
+a % b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a == b
+a != b
+a > b
+a < b
+!a && !c 
+!a || !c 
+*/
+
+//TASK 2----------------------------------------------------------------------------------
+
+//"My name is Your-first-name Your-last-name. You can contact me at your-email@mail.com.";
+//"My name is Sushant Vaid. You can contact me at Vaid0014@algonquinlive.com"
+
+// Declare variable first_name and assigning it with "Sushant".
+
+let first_name = "Sushant";
+
+// Declare variable last_name and assigning it with "Vaid"
+let last_name = "Vaid";
+
+// Declare variable mail and assigning it with "Vaid0014@algonquinlive.com"
+
+let mail = "Vaid0014@algonquinlive.com";
+
+let output= `My name is ${first_name} ${last_name}. You can contact me at ${mail}. `;
+alert(output);
+
+
+
+
